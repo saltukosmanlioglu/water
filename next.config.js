02349e117b06a000314975a98e9c86b9e3114de0
Saltuk Osmanlioglu <saltuk.osmanlioglu@gmail.com>
2022-06-21 18:33:42 +0200

    bump

--- next.config.js
+++ next.config.js
@@ -1,6 +1,26 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  pageExtensions: ["next.tsx"],
+  compiler: {
+    styledComponents: true,
+    relay: {
+      src: "./",
+      language: "typescript",
+    },
+  },
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/home",
+        permanent: true,
+      },
+    ];
+  },
+  devIndicators: {
+    buildActivity: false,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
---
+++ pages/home/index.next.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from "next";
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <div>Getir sucu</div>
+    </div>
+  );
+};
+
+export default Home;
